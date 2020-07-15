019368155deedd39c9bea2f09c5bf28c493e1257
嫌疑人侯某 <1297087292@qq.com>
2020-07-15 09:53:15 +0800

    创建分支

--- HelloGit/src/main/java/com/hou/MainClass.java
+++ HelloGit/src/main/java/com/hou/MainClass.java
@@ -5,5 +5,7 @@ public class MainClass {
 
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("更新一");
+        System.out.println("创建分支");
     }
 }
